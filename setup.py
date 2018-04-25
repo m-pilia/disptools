c5fba4eb00a5733e0bd39b6d013ade03754ce62f
Martino Pilia <martino.pilia@gmail.com>
2018-04-25 14:02:34 +0200

    v0.2.1

--- setup.py
+++ setup.py
@@ -111,7 +111,7 @@ disptools_c = Extension('_disptools',
                                'src/_disptools.c'
                                ])
 
-version = '0.2.0'
+version = '0.2.1'
 
 setup(name = 'disptools',
     packages = ['disptools'],
